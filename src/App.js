a6ecdea3553ea8bcb416f74fb5243da1ef614e7c
Pritam Maharjan <primgdev@gmail.com>
2025-02-03 18:42:17 +1100

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from './weather';
 
 function App() {
   return (
-    <div className='bg-gradient-to-r from-blue-400 to-indigo-600 h-screen'>
+    <div>
       <Weather />
     </div>
   );

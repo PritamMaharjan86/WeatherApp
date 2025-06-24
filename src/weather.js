067a24fe801621cfa8377efabddaf26f2777da78
Pritam Maharjan <primgdev@gmail.com>
2025-06-24 21:30:10 +1000

    adding features

--- src/weather.js
+++ src/weather.js
@@ -45,9 +45,9 @@ const Weather = () => {
         name: weatherReport.name,
         temp: (weatherReport.main.temp - 273.15).toFixed(1)
       }]);
-      setClicked(true);  
+      setClicked(true);
     } else {
-      setClicked(false);  
+      setClicked(false);
     }
   };
 
@@ -68,117 +68,144 @@ const Weather = () => {
     }
   };
 
+
+
   return (
-    <div className={`flex flex-col items-center h-screen ${getWeatherBackground()} bg-cover bg-center p-5`}>
+    <div
+      className={`flex flex-col items-center min-h-screen ${getWeatherBackground()} bg-cover bg-center p-4 sm:p-6 md:p-8`}
+    >
 
-      <div className="glass-card w-screen max-w-lg h-max p-8 bg-white shadow-2xl rounded-lg backdrop-blur-md">
-        <h1 className="font-amaranth text-5xl font-extrabold text-center h-max text-yellow-300 mb-6 tracking-widest drop-shadow-lg hover:drop-shadow-2xl transition-all duration-500 ease-in-out transform hover:scale-105 p-4 rounded-lg">
-          WEATHER APP
-        </h1>
 
-        {clicked && favorites.length > 0 && (
-          <div className="bg-white bg-opacity-50 p-4 rounded-lg shadow-lg mb-10">
-            <h2 className="text-2xl font-bold mb-4 text-yellow-200 font-amaranth">Favorite Locations</h2>
-            <ul className="space-y-2">
-              {favorites.map((fav, index) => (
-                <li key={index}>
-                  <span className='font-bold text-xl font-amaranth'>{fav.name} : </span>
-                  <span className="text-blue-400 text-xl font-bold font-amaranth">{fav.temp}°C</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+      <h1 className="font-amaranth text-3xl sm:text-4xl md:text-5xl font-extrabold text-center h-max text-yellow-300 mb-6 tracking-widest drop-shadow-lg hover:drop-shadow-2xl transition-all duration-500 ease-in-out transform hover:scale-105 p-4 rounded-lg">
+        WEATHER APP
+      </h1>
 
-        <div className="relative">
-          <input
-            value={location}
-            type="text"
-            onChange={handleLocation}
-            className="font-amaranth w-full mb-4 p-3 text-lg border rounded-lg shadow-sm bg-opacity-80 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500 pr-10"
-            placeholder="Enter a location"
-          />
 
-          {loading && (
-            <div className="absolute inset-y-0 right-0 flex items-center pr-3 pb-3">
-              <Loader />
-            </div>
-          )}
+      {clicked && favorites.length > 0 && (
+        <div className="bg-white bg-opacity-50 p-4 rounded-lg shadow-lg mb-10">
+          <h2 className="text-2xl font-bold mb-4 text-yellow-200 font-amaranth">Favorite Locations</h2>
+          <ul className="space-y-2">
+            {favorites.map((fav, index) => (
+              <li key={index}>
+                <span className='font-bold text-xl font-amaranth'>{fav.name} : </span>
+                <span className="text-blue-400 text-xl font-bold font-amaranth">{fav.temp}°C</span>
+              </li>
+            ))}
+          </ul>
         </div>
+      )}
 
-        <div className="flex justify-center items-center">
-          <button
-            onClick={handleWeather}
-            className="font-amaranth w-1/3 py-3 text-white bg-gradient-to-r from-blue-400 to-indigo-600 rounded-lg shadow-md hover:bg-gradient-to-r hover:from-blue-500 hover:to-indigo-700 transition-all font-bold text-xl "
-          >
-            Get Weather
-          </button>
-        </div>
+      <div className="relative">
+        <input
+          value={location}
+          type="text"
+          onChange={handleLocation}
+          className="font-amaranth w-full mb-4 p-3 text-lg border rounded-lg shadow-sm bg-opacity-80 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-500 pr-10"
+          placeholder="Enter a location"
+        />
 
-        {weatherReport.name && (
-          <div className="mt-4 p-4 bg-gray-200 bg-opacity-50 rounded-lg shadow-lg">
-
-            <CiStar
-              onClick={addToFavorites}
-              className={`text-2xl float-end font-bold ${clicked ? 'text-red-500' : 'text-yellow-200'}`}  
-            />
-
-            <div className="mb-4 text-center">
-              <p className="text-3xl text-yellow-200 font-amaranth p-5">{weatherReport.name}</p>
-              <p className="text-5xl font-bold text-blue-400 font-amaranth">
-                {(weatherReport.main.temp - 273.15).toFixed(1)}°C
-              </p>
-              <div className="flex justify-center">
-                <img
-                  src={`http://openweathermap.org/img/wn/${weatherReport.weather[0].icon}@2x.png`}
-                  alt="Weather icon"
-                />
-              </div>
-              <p className="text-xl font-amaranth">{weatherReport.weather[0].main}</p>
-            </div>
-
-            <div className="grid grid-cols-2 gap-4 pt-4 rounded-lg font-amaranth">
-              <div className="text-center">
-                <p className="text-lg ">Humidity</p>
-                <p className="text-2xl font-bold text-blue-400">{weatherReport.main.humidity}%</p>
-              </div>
-              <div className="text-center">
-                <p className="text-lg">Pressure</p>
-                <p className="text-2xl font-bold text-blue-400">{weatherReport.main.pressure} hPa</p>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-4 pt-4 rounded-lg font-amaranth">
-              <div className="text-center">
-                <p className="text-lg">Temp Max</p>
-                <p className="text-2xl font-bold text-blue-400">
-                  {(weatherReport.main.temp_max - 273.15).toFixed(1)}°C
-                </p>
-              </div>
-              <div className="text-center">
-                <p className="text-lg">Temp Min</p>
-                <p className="text-2xl font-bold text-blue-400">
-                  {(weatherReport.main.temp_min - 273.15).toFixed(1)}°C
-                </p>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-4 p-4 rounded-lg font-amaranth">
-              <div className="text-center">
-                <p className="text-lg">Wind Speed</p>
-                <p className="text-2xl font-bold text-blue-400">
-                  {weatherReport.wind.speed} m/s
-                </p>
-              </div>
-              <div className="text-center">
-                <p className="text-lg">Wind Direction</p>
-                <p className="text-2xl font-bold text-blue-400">{weatherReport.wind.deg}°</p>
-              </div>
-            </div>
+        {loading && (
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 pb-3">
+            <Loader />
           </div>
         )}
       </div>
+
+      <div className="flex justify-center items-center w-full">
+        <button
+          onClick={handleWeather}
+          className="font-amaranth w-4/5 sm:w-2/3 md:w-1/3 py-3 text-white bg-gradient-to-r from-blue-400 to-indigo-600 rounded-lg shadow-md hover:from-blue-500 hover:to-indigo-700 transition-all font-bold text-lg sm:text-xl"
+        >
+          Get Weather
+        </button>
+      </div>
+
+
+      {weatherReport.name && (
+        <div className="mt-4 p-4 bg-gray-200 bg-opacity-50 rounded-lg shadow-lg">
+
+          <CiStar
+            onClick={addToFavorites}
+            className={`text-2xl float-end font-bold ${clicked ? 'text-red-500' : 'text-yellow-200'}`}
+          />
+
+          <div className="mb-4 text-center">
+            <p className="text-3xl text-yellow-200 font-amaranth p-5">{weatherReport.name}</p>
+            <p className="text-5xl font-bold text-blue-400 font-amaranth">
+              {(weatherReport.main.temp - 273.15).toFixed(1)}°C
+            </p>
+            <div className="flex justify-center">
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherReport.weather[0].icon}@2x.png`}
+                alt="Weather icon"
+              />
+            </div>
+            <p className="text-xl font-amaranth">{weatherReport.weather[0].main}</p>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 pt-4 rounded-lg font-amaranth">
+            <div className="text-center">
+              <p className="text-lg ">Sunrise</p> <p className="text-2xl font-bold text-blue-400">{
+                new Intl.DateTimeFormat('en-US', {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  hour12: true,
+                  timeZone: 'UTC'
+                }).format((weatherReport.sys.sunrise + weatherReport.timezone) * 1000)
+              }</p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg ">Sunset</p>
+              <p className="text-2xl font-bold text-blue-400">{
+                new Intl.DateTimeFormat('en-US', {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  hour12: true,
+                  timeZone: 'UTC'
+                }).format((weatherReport.sys.sunset + weatherReport.timezone) * 1000)
+              }</p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg ">Humidity</p>
+              <p className="text-2xl font-bold text-blue-400">{weatherReport.main.humidity}%</p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg">Pressure</p>
+              <p className="text-2xl font-bold text-blue-400">{weatherReport.main.pressure} hPa</p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 pt-4 rounded-lg font-amaranth">
+            <div className="text-center">
+              <p className="text-lg">Temp Max</p>
+              <p className="text-2xl font-bold text-blue-400">
+                {(weatherReport.main.temp_max - 273.15).toFixed(1)}°C
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg">Temp Min</p>
+              <p className="text-2xl font-bold text-blue-400">
+                {(weatherReport.main.temp_min - 273.15).toFixed(1)}°C
+              </p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 p-4 rounded-lg font-amaranth">
+            <div className="text-center">
+              <p className="text-lg">Wind Speed</p>
+              <p className="text-2xl font-bold text-blue-400">
+                {weatherReport.wind.speed} m/s
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-lg">Wind Direction</p>
+              <p className="text-2xl font-bold text-blue-400">{weatherReport.wind.deg}°</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
+
   );
 };
 
